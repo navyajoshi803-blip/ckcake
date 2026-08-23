1eaeeb0c18dd81221b77b8b4b67ed7027e7328fa
navyajoshi803-blip <navyajoshi803@gmail.com>
2026-08-24 02:21:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
